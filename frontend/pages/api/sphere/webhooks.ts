ef31ad2e6f6e21556f02230f37ff37b8cffaf943
kamalbuilds <geniusamansingh@gmail.com>
2023-08-20 01:48:10 +0530

    webhook listen

--- frontend/pages/api/sphere/webhooks.ts
+++ frontend/pages/api/sphere/webhooks.ts
@@ -3,18 +3,20 @@ import crypto from "crypto";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (process.env.SPHERE_WEBHOOK_SECRET) {
-    const signature = crypto
-      .createHmac("sha256", Buffer.from(process.env.SPHERE_WEBHOOK_SECRET!))
-      .update(JSON.stringify(req.body), "utf8")
-      .digest("hex");
+  // if (process.env.SPHERE_WEBHOOK_SECRET) {
+  //   const signature = crypto
+  //     .createHmac("sha256", Buffer.from(process.env.SPHERE_WEBHOOK_SECRET!))
+  //     .update(JSON.stringify(req.body), "utf8")
+  //     .digest("hex");
 
-    if (signature !== req.headers["signature"]) {
-      return res.status(403).json({ message: "Not OK" });
-    }
-  }
+  //   if (signature !== req.headers["signature"]) {
+  //     return res.status(403).json({ message: "Not OK" });
+  //   }
+  // }
 
   if (req.method === "POST") {
+    console.log(req.body);
+
     const projectId =
       req.body.data.payment.paymentLink.lineItems[0].price.product.meta
         .projectId;
