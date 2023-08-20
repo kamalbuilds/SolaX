f5e94ab1f11ac5c8a3f79513a0cf522c5d127339
kamalbuilds <geniusamansingh@gmail.com>
2023-08-20 23:07:40 +0530

    converting to axios

--- frontend/pages/api/sphere/webhooks.ts
+++ frontend/pages/api/sphere/webhooks.ts
@@ -1,7 +1,7 @@
 import { underdogClient } from "../../../lib/underdog";
 import crypto from "crypto";
 import { NextApiRequest, NextApiResponse } from "next";
-
+import axios from 'axios';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
@@ -13,31 +13,29 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (receiverAddress) {
       // const receiverAddress = req.body.data.payment.customer.solanaPubKey;
 
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("x-api-key", process.env.NEXT_PUBLIC_SHYFT_API_KEY);
+        
+    const headers = {
+      'Content-Type': 'application/json',
+      'x-api-key': process.env.NEXT_PUBLIC_SHYFT_API_KEY
+    };
 
-        const raw = JSON.stringify({
-          network: "devnet",
-          nft_address: "G4EpMRJirDGFcyq7eR9VZ31nPgDhKLpD3HjpZ8GXTzVm",
-          sender: "BFefyp7jNF5Xq2A4JDLLFFGpxLq5oPEFKBAQ46KJHW2R",
-          receiver: receiverAddress,
-        });
+    const data = {
+      network: 'devnet',
+      nft_address: 'G4EpMRJirDGFcyq7eR9VZ31nPgDhKLpD3HjpZ8GXTzVm',
+      sender: 'BFefyp7jNF5Xq2A4JDLLFFGpxLq5oPEFKBAQ46KJHW2R',
+      receiver: receiverAddress
+    };
 
-        const requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: raw,
-          redirect: 'follow'
-        };
+    const url = 'https://api.shyft.to/sol/v1/nft/compressed/transfer';
 
-        try {
-          const response = await fetch("https://api.shyft.to/sol/v1/nft/compressed/transfer", requestOptions);
-          const result = await response.text();
-          console.log(result);
-        } catch (error) {
-          console.log('error', error);
-        }
+    axios.post(url, data, { headers })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log('error', error);
+      });
+
         return res.status(200).json({ message: "OK" });
     }
   }
